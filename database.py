a54744eaac5d2adff31df28314531d3cbfb2316b
duhov <portoslunt@gmail.com>
2024-12-30 16:54:59 +0300

    project database manager

--- database.py
+++ database.py
@@ -5,14 +5,16 @@ def get_students():
     db = sqlite3.connect('db.sqlite')
     cur = db.cursor()
     rows = cur.execute('''SELECT rowid, name, age, information FROM STUDENTS''')
+    db.commit()
     cur.close()
+    db.close()
     students = {}
     for row in rows:
-        id = row[-1]
+        id = row[0]
         other = {
-            'name': row[0],
-            'age': row[1],
-            'information': row[2]
+            'name': row[1],
+            'age': row[2],
+            'information': row[3]
         }
         students[id] = other
     return students
@@ -20,7 +22,9 @@ def get_student(id):
     db = sqlite3.connect('db.sqlite')
     cur = db.cursor()
     row = cur.execute(f'''SELECT name, age, information, rowid FROM STUDENTS WHERE rowid = {id}''')[0]
+    db.commit()
     cur.close()
+    db.close()
     return {
         'name': row[0],
         'age': row[1],
@@ -35,20 +39,29 @@ name string,
 age integer,
 information string
 )''')
+    db.commit()
+    cur.close()
+    db.close()
 def edit_student(id,name=None,age=None,information=None):
     db = sqlite3.connect('db.sqlite')
     cur = db.cursor()
     for parameter,value in zip(['name','age','information'],[name,age,information]):
         if value:
             cur.execute(f'UPDATE STUDENTS SET {parameter} WHERE rowid = {id}')
+    db.commit()
     cur.close()
+    db.close()
 def remove_student(id):
     db = sqlite3.connect('db.sqlite')
     cur = db.cursor()
     cur.execute(f'DELETE FROM STUDENTS WHERE rowid = {id}')
+    db.commit()
     cur.close()
+    db.close()
 def add_student(name, age, information):
     db = sqlite3.connect('db.sqlite')
     cur = db.cursor()
     cur.execute(f"INSERT INTO STUDENTS (name, age, information) VALUES ('{name}', {age}, '{information}')")
+    db.commit()
     cur.close()
+    db.close()
