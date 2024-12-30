11a80aa61b99f28277d8b320123bb1e6923258c8
duhov <portoslunt@gmail.com>
2024-12-30 16:51:41 +0300

    project database manager

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ import os
 def get_students():
     db = sqlite3.connect('db.sqlite')
     cur = db.cursor()
-    rows = cur.execute('''SELECT name, age, information, ROWID FROM STUDENTS''')
+    rows = cur.execute('''SELECT rowid, name, age, information FROM STUDENTS''')
     cur.close()
     students = {}
     for row in rows:
@@ -19,7 +19,7 @@ def get_students():
 def get_student(id):
     db = sqlite3.connect('db.sqlite')
     cur = db.cursor()
-    row = cur.execute(f'''SELECT name, age, information, ROWID FROM STUDENTS WHERE ROWID = {id}''')[0]
+    row = cur.execute(f'''SELECT name, age, information, rowid FROM STUDENTS WHERE rowid = {id}''')[0]
     cur.close()
     return {
         'name': row[0],
@@ -40,12 +40,12 @@ def edit_student(id,name=None,age=None,information=None):
     cur = db.cursor()
     for parameter,value in zip(['name','age','information'],[name,age,information]):
         if value:
-            cur.execute(f'UPDATE STUDENTS SET {parameter} WHERE ROWID = {id}')
+            cur.execute(f'UPDATE STUDENTS SET {parameter} WHERE rowid = {id}')
     cur.close()
 def remove_student(id):
     db = sqlite3.connect('db.sqlite')
     cur = db.cursor()
-    cur.execute(f'DELETE FROM STUDENTS WHERE ROWID = {id}')
+    cur.execute(f'DELETE FROM STUDENTS WHERE rowid = {id}')
     cur.close()
 def add_student(name, age, information):
     db = sqlite3.connect('db.sqlite')
